6f9f2e19fe5408cce537363fa17ad3448783e607
lyw1024 <almostloverv@yahoo.com>
2020-11-21 18:48:13 +0700

    add login page

---
+++ pages/login/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        Login
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
