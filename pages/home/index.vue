d0f2f7a9817f03ba4983905c1393ed345df50168
junyuZhou <feijifeibudao@gmail.com>
2020-11-21 20:48:46 +0800

    add pages

---
+++ pages/home/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        Home
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+}
+</style>
